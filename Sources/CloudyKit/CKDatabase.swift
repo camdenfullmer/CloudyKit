77519376745eb452120e3eedda63656c6cf1be8c
Camden Fullmer <camdenfullmer@gmail.com>
2020-12-24 19:57:03 -0500

    Replace data tasks with data task publishers.

--- Sources/CloudyKit/CKDatabase.swift
+++ Sources/CloudyKit/CKDatabase.swift
@@ -30,7 +30,7 @@ public class CKDatabase {
         self.databaseScope = databaseScope
     }
     
-    var cancelable: AnyCancellable? = nil
+    var cancellable: AnyCancellable? = nil
     
     public func save(_ record: CKRecord, completionHandler: @escaping (CKRecord?, Error?) -> Void) {        
 //        let assetFieldDictionarys: [CKWSAssetFieldDictionary] = record.fields.compactMap { fieldName, value in
@@ -48,30 +48,71 @@ public class CKDatabase {
 //            }
 //            task.resume()
 //        }
-        let task = CloudyKitConfig.urlSession.saveTask(database: self,
-                                                       environment: CloudyKitConfig.environment,
-                                                       record: record,
-                                                       completionHandler: completionHandler)
-        task.resume()
+        self.cancellable = CloudyKitConfig.urlSession.saveTaskPublisher(database: self, environment: CloudyKitConfig.environment, record: record)
+            .decode(type: CKWSRecordResponse.self, decoder: CloudyKitConfig.decoder)
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    completionHandler(nil, error)
+                }
+            }, receiveValue: { response in
+                guard let responseRecord = response.records.first,
+                      let recordType = responseRecord.recordType,
+                      let createdTimestamp = responseRecord.created?.timestamp else {
+                    completionHandler(nil, CKError(code: .internalError))
+                    return
+                }
+                let id = CKRecord.ID(recordName: responseRecord.recordName)
+                let record = CKRecord(recordType: recordType, recordID: id)
+                record.creationDate = Date(timeIntervalSince1970: TimeInterval(createdTimestamp) / 1000)
+                record.recordChangeTag = responseRecord.recordChangeTag
+                for (fieldName, fieldValue) in responseRecord.fields ?? [:] {
+                    switch fieldValue.value {
+                    case .string(let value): record[fieldName] = value
+                    case .number(let value): record[fieldName] = value
+                    case .asset(_): fatalError("not supported")
+                    }
+                }
+                completionHandler(record, nil)
+            })
     }
     
     public func fetch(withRecordID recordID: CKRecord.ID, completionHandler: @escaping (CKRecord?, Error?) -> Void) {
-        let task = CloudyKitConfig.urlSession.fetchTask(database: self,
-                                                        environment: CloudyKitConfig.environment,
-                                                        recordID: recordID,
-                                                        completionHandler: completionHandler)
-        task.resume()
+        self.cancellable = CloudyKitConfig.urlSession.fetchTaskPublisher(database: self, environment: CloudyKitConfig.environment, recordID: recordID)
+            .decode(type: CKWSRecordResponse.self, decoder: CloudyKitConfig.decoder)
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    completionHandler(nil, error)
+                }
+            }, receiveValue: { response in
+                guard let responseRecord = response.records.first,
+                      let recordType = responseRecord.recordType,
+                      let createdTimestamp = responseRecord.created?.timestamp else {
+                    completionHandler(nil, CKError(code: .internalError))
+                    return
+                }
+                let id = CKRecord.ID(recordName: responseRecord.recordName)
+                let record = CKRecord(recordType: recordType, recordID: id)
+                record.creationDate = Date(timeIntervalSince1970: TimeInterval(createdTimestamp) / 1000)
+                record.recordChangeTag = responseRecord.recordChangeTag
+                for (fieldName, fieldValue) in responseRecord.fields ?? [:] {
+                    switch fieldValue.value {
+                    case .string(let value): record[fieldName] = value
+                    case .number(let value): record[fieldName] = value
+                    case .asset(_): fatalError("not supported")
+                    }
+                }
+                completionHandler(record, nil)
+            })
     }
     
     public func delete(withRecordID recordID: CKRecord.ID, completionHandler: @escaping (CKRecord.ID?, Error?) -> Void) {
-        self.cancelable = CloudyKitConfig.urlSession.deleteTaskPublisher(database: self, environment: CloudyKitConfig.environment, recordID: recordID)
-            .tryMap { output in
-                // TODO: Handle error better
-                guard let response = output.response as? HTTPURLResponse, response.statusCode == 200 else {
-                    throw CKError(code: .internalError)
-                }
-                return output.data
-            }
+        self.cancellable = CloudyKitConfig.urlSession.deleteTaskPublisher(database: self, environment: CloudyKitConfig.environment, recordID: recordID)
             .decode(type: CKWSRecordResponse.self, decoder: CloudyKitConfig.decoder)
             .sink(receiveCompletion: { completion in
                 switch completion {
--- Sources/CloudyKit/URLSession+Helpers.swift
+++ Sources/CloudyKit/URLSession+Helpers.swift
@@ -38,130 +38,33 @@ extension URLSession: NetworkSession {
 extension URLSessionDataTask: NetworkSessionDataTask { }
 
 extension NetworkSession {
-    internal func successfulDataTask(with request: URLRequest, completionHandler: @escaping (Data?, Error?) -> Void) -> NetworkSessionDataTask {
-        return self.internalDataTask(with: request) { data, response, error in
-            if let error = error {
-                completionHandler(nil, error)
-                return
-            }
-            guard let response = response as? HTTPURLResponse else {
-                completionHandler(nil, CKError(code: .internalError))
-                return
-            }
-            if let data = data {
+    internal func successfulDataTaskPublisher(for request: URLRequest) -> AnyPublisher<Data, Error> {
+        return self.internalDataTaskPublisher(for: request)
+            .tryMap { output in
+                guard let response = output.response as? HTTPURLResponse else {
+                    throw CKError(code: .internalError)
+                }
                 if CloudyKitConfig.debug {
                     print("=== CloudKit Web Services Response ===")
                     print("Status Code: \(response.statusCode)")
                     print("Data:")
-                    print("\(String(data: data, encoding: .utf8) ?? "invalid data")")
+                    print("\(String(data: output.data, encoding: .utf8) ?? "invalid data")")
                     print("======================================")
                 }
-            }
-            guard response.statusCode == 200 else {
-                if let data = data {
-                    if let ckwsError = try? CloudyKitConfig.decoder.decode(CKWSErrorResponse.self, from: data) {
-                        if CloudyKitConfig.debug {
-                            print("error: \(ckwsError)")
-                        }
-                        completionHandler(nil, ckwsError.ckError)
-                        return
-                    }
-                    completionHandler(nil, CKError(code: .internalError))
-                    return
-                }
-                // TODO: Handle error
-                completionHandler(nil, CKError(code: .internalError))
-                return
-            }
-            guard let data = data else {
-                // TODO: Handle error
-                completionHandler(nil, CKError(code: .internalError))
-                return
-            }
-            if let ckwsError = try? CloudyKitConfig.decoder.decode(CKWSErrorResponse.self, from: data) {
-                completionHandler(nil, ckwsError.ckError)
-                return
-            }
-            
-            completionHandler(data, nil)
-        }
-    }
-    
-    internal func recordResponseTask(with request: URLRequest, completionHandler: @escaping (CKRecord?, Error?) -> Void) -> NetworkSessionDataTask {
-        return self.successfulDataTask(with: request) { (data, error) in
-            if let error = error {
-                completionHandler(nil, error)
-            }
-            if let data = data {
-                do {
-                    let response = try CloudyKitConfig.decoder.decode(CKWSRecordResponse.self, from: data)
-                    guard let responseRecord = response.records.first,
-                          let recordType = responseRecord.recordType,
-                          let createdTimestamp = responseRecord.created?.timestamp else {
-                        completionHandler(nil, CKError(code: .internalError))
-                        return
-                    }
-                    let id = CKRecord.ID(recordName: responseRecord.recordName)
-                    let record = CKRecord(recordType: recordType, recordID: id)
-                    record.creationDate = Date(timeIntervalSince1970: TimeInterval(createdTimestamp) / 1000)
-                    record.recordChangeTag = responseRecord.recordChangeTag
-                    for (fieldName, fieldValue) in responseRecord.fields ?? [:] {
-                        switch fieldValue.value {
-                        case .string(let value): record[fieldName] = value
-                        case .number(let value): record[fieldName] = value
-                        case .asset(_): fatalError("not supported")
-                        }
-                    }
-                    completionHandler(record, nil)
-                } catch {
-                    completionHandler(nil, error)
-                }
-            }
-        }
-    }
-    
-    internal func recordIDResponseTask(with request: URLRequest, completionHandler: @escaping (CKRecord.ID?, Error?) -> Void) -> NetworkSessionDataTask {
-        return self.successfulDataTask(with: request) { (data, error) in
-            if let error = error {
-                completionHandler(nil, error)
-            }
-            if let data = data {
-                do {
-                    let response = try CloudyKitConfig.decoder.decode(CKWSRecordResponse.self, from: data)
-                    guard let responseRecord = response.records.first else {
-                        completionHandler(nil, CKError(code: .internalError))
-                        return
-                    }
-                    let id = CKRecord.ID(recordName: responseRecord.recordName)
-                    completionHandler(id, nil)
-                } catch {
+                if let ckwsError = try? CloudyKitConfig.decoder.decode(CKWSErrorResponse.self, from: output.data) {
                     if CloudyKitConfig.debug {
-                        print("error decoding: \(error.localizedDescription)")
-                        print("data: \(String(data: data, encoding: .utf8) ?? "no data")")
+                        print("error: \(ckwsError)")
                     }
-                    completionHandler(nil, error)
+                    throw ckwsError.ckError
                 }
-            }
-        }
+                guard response.statusCode == 200 else {
+                    throw CKError(code: .internalError)
+                }
+                return output.data
+            }.eraseToAnyPublisher()
     }
     
-    internal func modelResponseTask<Model: Decodable>(with request: URLRequest, completionHandler: @escaping (Model?, Error?) -> Void) -> NetworkSessionDataTask {
-        return self.successfulDataTask(with: request) { (data, error) in
-            if let error = error {
-                completionHandler(nil, error)
-            }
-            if let data = data {
-                do {
-                    let model = try CloudyKitConfig.decoder.decode(Model.self, from: data)
-                    completionHandler(model, nil)
-                } catch {
-                    completionHandler(nil, error)
-                }
-            }
-        }
-    }
-    
-    internal func saveTask(database: CKDatabase, environment: CloudyKitConfig.Environment, record: CKRecord, completionHandler: @escaping (CKRecord?, Error?) -> Void) -> NetworkSessionDataTask {
+    internal func saveTaskPublisher(database: CKDatabase, environment: CloudyKitConfig.Environment, record: CKRecord) -> AnyPublisher<Data, Error> {
         let now = Date()
         let path = "/database/1/\(database.containerIdentifier)/\(environment.rawValue)/\(database.databaseScope.description)/records/modify"
         var request = URLRequest(url: URL(string: "\(CloudyKitConfig.host)\(path)")!)
@@ -199,10 +102,10 @@ extension NetworkSession {
             }
             request.httpBody = data
         }
-        return self.recordResponseTask(with: request, completionHandler: completionHandler)
+        return self.successfulDataTaskPublisher(for: request)
     }
     
-    internal func fetchTask(database: CKDatabase, environment: CloudyKitConfig.Environment, recordID: CKRecord.ID, completionHandler: @escaping (CKRecord?, Error?) -> Void) -> NetworkSessionDataTask {
+    internal func fetchTaskPublisher(database: CKDatabase, environment: CloudyKitConfig.Environment, recordID: CKRecord.ID) -> AnyPublisher<Data, Error> {
         let now = Date()
         let path = "/database/1/\(database.containerIdentifier)/\(environment.rawValue)/\(database.databaseScope.description)/records/lookup"
         var request = URLRequest(url: URL(string: "\(CloudyKitConfig.host)\(path)")!)
@@ -221,10 +124,10 @@ extension NetworkSession {
             }
             request.httpBody = data
         }
-        return self.recordResponseTask(with: request, completionHandler: completionHandler)
+        return self.successfulDataTaskPublisher(for: request)
     }
     
-    internal func deleteTaskPublisher(database: CKDatabase, environment: CloudyKitConfig.Environment, recordID: CKRecord.ID) -> AnyPublisher<(data: Data, response: URLResponse), Error> {
+    internal func deleteTaskPublisher(database: CKDatabase, environment: CloudyKitConfig.Environment, recordID: CKRecord.ID) -> AnyPublisher<Data, Error> {
         let now = Date()
         let path = "/database/1/\(database.containerIdentifier)/\(environment.rawValue)/\(database.databaseScope.description)/records/modify"
         var request = URLRequest(url: URL(string: "\(CloudyKitConfig.host)\(path)")!)
@@ -249,38 +152,10 @@ extension NetworkSession {
             }
             request.httpBody = data
         }
-        return CloudyKitConfig.urlSession.internalDataTaskPublisher(for: request)
+        return self.successfulDataTaskPublisher(for: request)
     }
-    
-    internal func deleteTask(database: CKDatabase, environment: CloudyKitConfig.Environment, recordID: CKRecord.ID, completionHandler: @escaping (CKRecord.ID?, Error?) -> Void) -> NetworkSessionDataTask {
-        let now = Date()
-        let path = "/database/1/\(database.containerIdentifier)/\(environment.rawValue)/\(database.databaseScope.description)/records/modify"
-        var request = URLRequest(url: URL(string: "\(CloudyKitConfig.host)\(path)")!)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue(CloudyKitConfig.serverKeyID, forHTTPHeaderField: "X-Apple-CloudKit-Request-KeyID")
-        request.addValue(CloudyKitConfig.dateFormatter.string(from: now), forHTTPHeaderField: "X-Apple-CloudKit-Request-ISO8601Date")
-        let recordDictionary = CKWSRecordDictionary(recordName: recordID.recordName,
-                                                    recordType: nil,
-                                                    recordChangeTag: nil,
-                                                    fields: nil,
-                                                    created: nil)
-        let operationType: CKWSRecordOperation.OperationType = .forceDelete
-        let operation = CKWSRecordOperation(operationType: operationType,
-                                            desiredKeys: nil,
-                                            record: recordDictionary)
-        let modifyRequest = CKWSModifyRecordRequest(operations: [operation])
-        if let data = try? CloudyKitConfig.encoder.encode(modifyRequest), let privateKey = CloudyKitConfig.serverPrivateKey {
-            let signature = CKRequestSignature(data: data, date: now, path: path, privateKey: privateKey)
-            if let signatureValue = try? signature.sign() {
-                request.addValue(signatureValue, forHTTPHeaderField: "X-Apple-CloudKit-Request-SignatureV1")
-            }
-            request.httpBody = data
-        }
-        return self.recordIDResponseTask(with: request, completionHandler: completionHandler)
-    }
-    
-    internal func requestAssetTokenTask(database: CKDatabase, environment: CloudyKitConfig.Environment, tokenRequest: CKWSAssetTokenRequest, completionHandler: @escaping ([CKWSTokenResponseDictionary]?, Error?) -> Void) -> NetworkSessionDataTask {
+
+    internal func requestAssetTokenTaskPublisher(database: CKDatabase, environment: CloudyKitConfig.Environment, tokenRequest: CKWSAssetTokenRequest) -> AnyPublisher<Data, Error> {
         let now = Date()
         let path = "/database/1/\(database.containerIdentifier)/\(environment.rawValue)/\(database.databaseScope.description)/assets/upload"
         var request = URLRequest(url: URL(string: "\(CloudyKitConfig.host)\(path)")!)
@@ -295,6 +170,6 @@ extension NetworkSession {
             }
             request.httpBody = data
         }
-        return self.modelResponseTask(with: request, completionHandler: completionHandler)
+        return self.successfulDataTaskPublisher(for: request)
     }
 }
