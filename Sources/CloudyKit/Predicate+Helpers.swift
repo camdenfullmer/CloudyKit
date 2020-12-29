2c994373f15608f87a4d9f36210752230d10bd10
Camden Fullmer <camdenfullmer@gmail.com>
2020-12-28 20:10:20 -0500

    Add support for date and other comparators.

--- Sources/CloudyKit/Predicate+Helpers.swift
+++ Sources/CloudyKit/Predicate+Helpers.swift
@@ -10,7 +10,7 @@ import Foundation
 #if os(Linux)
 public typealias Predicate = CKPredicate
 #else
-public typealias Predicate = CKPredicate
+public typealias Predicate = NSPredicate
 #endif
 
 public class CKPredicate {
@@ -26,16 +26,20 @@ public class CKPredicate {
         let splits: [String] = predicateFormat.components(separatedBy: "%@")
         var substitution = ""
         for (index, split) in splits.enumerated() {
+            // Replace "%@" with objects from the arguments array.
             if index > 0, arguments.count > 0 {
                 let argument = argumentsLeft.removeFirst()
                 switch argument {
                 case is String:
                     substitution += "\"\(argument)\""
+                case let date as NSDate:
+                    substitution += "CAST(\(date.timeIntervalSinceReferenceDate), \"NSDate\")"
                 default:
                     substitution += "\(argument)"
                 }
             }
             let keyPathSplits = split.components(separatedBy: "%K")
+            // Replace "%K" with field names from the arguments array.
             if keyPathSplits.count > 1 {
                 for (keyPathIndex, keyPathSplit) in keyPathSplits.enumerated() {
                     if keyPathIndex > 0, arguments.count > 0 {
@@ -76,6 +80,10 @@ extension Predicate {
                         fieldValue = CKWSRecordFieldValue(value: .reference(referenceDictionary), type: nil)
                     }
                 }
+            } else if field.hasPrefix("CAST("), field.hasSuffix(", \"NSDate\")"), let value = field.components(separatedBy: "CAST(").last?.components(separatedBy: ", \"NSDate\")").first, let timeInterval = Double(value) {
+                let date = Date(timeIntervalSinceReferenceDate: timeInterval)
+                let timeInterval = date.timeIntervalSince1970
+                fieldValue = CKWSRecordFieldValue(value: .dateTime(Int(timeInterval * 1000)), type: nil)
             } else if (field.hasPrefix("ANY {") || field.hasPrefix("{")) && field.hasSuffix("}"){
                 comparator = .in
                 let firstSeparation = field.hasPrefix("{") ? "{" : "ANY {"
@@ -88,6 +96,8 @@ extension Predicate {
                 fieldValue = CKWSRecordFieldValue(value: .stringList(listValues), type: nil)
             } else if field.hasPrefix("\"") && field.hasSuffix("\"") {
                 fieldValue = CKWSRecordFieldValue(value: .string(field.trimmingCharacters(in: CharacterSet(["\""]))), type: nil)
+            } else if let number = Int(field) {
+                fieldValue = CKWSRecordFieldValue(value: .number(number), type: nil)
             } else {
                 if field.hasPrefix("ANY ") {
                     if originalComparator == .equals {
@@ -112,26 +122,83 @@ extension Predicate {
     
     private func comparatorWithFields() -> (CKWSFilterDictionary.Comparator, [String]) {
         let doubleEqualsSplits = self.predicateFormat.components(separatedBy: "==")
-        let equalsSplits = self.predicateFormat.components(separatedBy: "=")
-        let containsSplits = self.predicateFormat.components(separatedBy: "CONTAINS")
-        let beginsWithSplits = self.predicateFormat.components(separatedBy: "BEGINSWITH")
-        let inSplits = self.predicateFormat.components(separatedBy: "IN")
         if doubleEqualsSplits.count == 2 {
             let fields = doubleEqualsSplits.compactMap { String($0).trimmingCharacters(in: .whitespaces) }
             return (.equals, fields)
-        } else if equalsSplits.count == 2 {
-            let fields = equalsSplits.compactMap { String($0).trimmingCharacters(in: .whitespaces) }
-            return (.equals, fields)
-        } else if containsSplits.count == 2 {
-            let fields = containsSplits.compactMap { String($0).trimmingCharacters(in: .whitespaces) }
-            return (.listContains, fields)
-        } else if beginsWithSplits.count == 2 {
-            let fields = beginsWithSplits.compactMap { String($0).trimmingCharacters(in: .whitespaces) }
-            return (.beginsWith, fields)
-        } else if inSplits.count == 2 {
+        }
+        
+        let greaterThanOrEqualsSplits = self.predicateFormat.components(separatedBy: ">=")
+        if greaterThanOrEqualsSplits.count == 2 {
+            let fields = greaterThanOrEqualsSplits.compactMap { String($0).trimmingCharacters(in: .whitespaces) }
+            return (.greaterThanOrEquals, fields)
+        }
+    
+        let equalsOrGreaterThanSplits = self.predicateFormat.components(separatedBy: "=>")
+        if equalsOrGreaterThanSplits.count == 2 {
+            let fields = equalsOrGreaterThanSplits.compactMap { String($0).trimmingCharacters(in: .whitespaces) }
+            return (.greaterThanOrEquals, fields)
+        }
+        
+        let lessThanOrEqualsSplits = self.predicateFormat.components(separatedBy: "<=")
+        if lessThanOrEqualsSplits.count == 2 {
+            let fields = lessThanOrEqualsSplits.compactMap { String($0).trimmingCharacters(in: .whitespaces) }
+            return (.lessThanOrEquals, fields)
+        }
+        
+        let equalsOrLessThanSplits = self.predicateFormat.components(separatedBy: "=<")
+        if equalsOrLessThanSplits.count == 2 {
+            let fields = equalsOrLessThanSplits.compactMap { String($0).trimmingCharacters(in: .whitespaces) }
+            return (.lessThanOrEquals, fields)
+        }
+        
+        let notEquals = self.predicateFormat.components(separatedBy: "!=")
+        if notEquals.count == 2 {
+            let fields = notEquals.compactMap { String($0).trimmingCharacters(in: .whitespaces) }
+            return (.notEquals, fields)
+        }
+        
+        let 🥕🥕Splits = self.predicateFormat.components(separatedBy: "<>")
+        if 🥕🥕Splits.count == 2 {
+            let fields = 🥕🥕Splits.compactMap { String($0).trimmingCharacters(in: .whitespaces) }
+            return (.notEquals, fields)
+        }
+        
+        let equalsSplits = self.predicateFormat.components(separatedBy: "=")
+        if equalsSplits.count == 2 {
+           let fields = equalsSplits.compactMap { String($0).trimmingCharacters(in: .whitespaces) }
+           return (.equals, fields)
+        }
+        
+        let greaterThanSplits = self.predicateFormat.components(separatedBy: ">")
+        if greaterThanSplits.count == 2 {
+           let fields = greaterThanSplits.compactMap { String($0).trimmingCharacters(in: .whitespaces) }
+           return (.greaterThan, fields)
+        }
+        
+        let lessThanSplits = self.predicateFormat.components(separatedBy: "<")
+        if lessThanSplits.count == 2 {
+           let fields = lessThanSplits.compactMap { String($0).trimmingCharacters(in: .whitespaces) }
+           return (.lessThan, fields)
+        }
+        
+        let containsSplits = self.predicateFormat.components(separatedBy: "CONTAINS")
+        if containsSplits.count == 2 {
+           let fields = containsSplits.compactMap { String($0).trimmingCharacters(in: .whitespaces) }
+           return (.listContains, fields)
+        }
+        
+        let beginsWithSplits = self.predicateFormat.components(separatedBy: "BEGINSWITH")
+        if beginsWithSplits.count == 2 {
+           let fields = beginsWithSplits.compactMap { String($0).trimmingCharacters(in: .whitespaces) }
+           return (.beginsWith, fields)
+        }
+        
+        let inSplits = self.predicateFormat.components(separatedBy: "IN")
+        if inSplits.count == 2 {
             let fields = inSplits.compactMap { String($0).trimmingCharacters(in: .whitespaces) }
             return (.in, fields)
         }
+        
         fatalError("invalid predicate: \(self.predicateFormat)")
     }
     
--- Tests/CloudyKitTests/Predicate+HelpersTests.swift
+++ Tests/CloudyKitTests/Predicate+HelpersTests.swift
@@ -13,17 +13,7 @@ import FoundationNetworking
 
 final class PredicatePlusHelpersTests: XCTestCase {
     
-    func testEqualsReference() {
-        let recordID = CKRecord.ID(recordName: UUID().uuidString)
-        let reference = CKRecord.Reference(recordID: recordID, action: .none)
-        let predicate = Predicate(format: "employee == %@", reference)
-        let filterBy = predicate.filterBy
-        XCTAssertEqual([
-            CKWSFilterDictionary(comparator: .equals, fieldName: "employee", fieldValue: CKWSRecordFieldValue(value: .reference(CKWSReferenceDictionary(recordName: recordID.recordName, action: "NONE")), type: nil))
-        ], filterBy)
-    }
-    
-    func testMatchFieldToSpecificValue() {
+    func testFieldContainsSpecificValue() {
         let predicates = [
             Predicate(format: "ANY favoriteColors = 'red'"),
             Predicate(format: "favoriteColors CONTAINS 'red'"),
@@ -36,6 +26,23 @@ final class PredicatePlusHelpersTests: XCTestCase {
                 CKWSFilterDictionary(comparator: .listContains, fieldName: "favoriteColors", fieldValue: CKWSRecordFieldValue(value: .string("red"), type: nil))
             ], predicate.filterBy)
         }
+    }
+    
+    func testMatchFieldToSpecificValue() {
+        let numberPredicate = Predicate(format: "29 = age")
+        XCTAssertEqual([CKWSFilterDictionary(comparator: .equals, fieldName: "age", fieldValue: CKWSRecordFieldValue(value: .number(29), type: nil))], numberPredicate.filterBy)
+        
+        let date = NSDate(timeIntervalSince1970: 123456789)
+        let datePredicate = Predicate(format: "today == %@", date)
+        XCTAssertEqual([CKWSFilterDictionary(comparator: .equals, fieldName: "today", fieldValue: CKWSRecordFieldValue(value: .dateTime(Int(date.timeIntervalSince1970 * 1000)), type: nil))], datePredicate.filterBy)
+        
+        let stringPredicate = Predicate(format: "'red' = favoriteColor")
+        XCTAssertEqual([CKWSFilterDictionary(comparator: .equals, fieldName: "favoriteColor", fieldValue: CKWSRecordFieldValue(value: .string("red"), type: nil))], stringPredicate.filterBy)
+
+        let recordID = CKRecord.ID(recordName: UUID().uuidString)
+        let reference = CKRecord.Reference(recordID: recordID, action: .none)
+        let referencePredicate = Predicate(format: "employee == %@", reference)
+        XCTAssertEqual([CKWSFilterDictionary(comparator: .equals, fieldName: "employee", fieldValue: CKWSRecordFieldValue(value: .reference(CKWSReferenceDictionary(recordName: recordID.recordName, action: "NONE")), type: nil))], referencePredicate.filterBy)
     }
     
     func testMatchFieldToOneOrMoreValues() {
@@ -64,16 +71,84 @@ final class PredicatePlusHelpersTests: XCTestCase {
         }
     }
     
+    func testEqualsComparator() {
+        let predicates = [
+            Predicate(format: "number = 13"),
+            Predicate(format: "number == 13"),
+        ]
+        for predicate in predicates {
+            XCTAssertEqual([
+                CKWSFilterDictionary(comparator: .equals, fieldName: "number", fieldValue: CKWSRecordFieldValue(value: .number(13), type: nil))
+            ], predicate.filterBy)
+        }
+    }
+    
+    func testGreaterThanOrEqualsComparator() {
+        let predicates = [
+            Predicate(format: "number >= 13"),
+            Predicate(format: "number => 13"),
+        ]
+        for predicate in predicates {
+            XCTAssertEqual([
+                CKWSFilterDictionary(comparator: .greaterThanOrEquals, fieldName: "number", fieldValue: CKWSRecordFieldValue(value: .number(13), type: nil))
+            ], predicate.filterBy)
+        }
+    }
+    
+    func testLessThanOrEqualsComparator() {
+        let predicates = [
+            Predicate(format: "number <= 13"),
+            Predicate(format: "number =< 13"),
+        ]
+        for predicate in predicates {
+            XCTAssertEqual([
+                CKWSFilterDictionary(comparator: .lessThanOrEquals, fieldName: "number", fieldValue: CKWSRecordFieldValue(value: .number(13), type: nil))
+            ], predicate.filterBy)
+        }
+    }
+    
+    func testGreaterThanComparator() {
+        let predicate = Predicate(format: "number > 13")
+        XCTAssertEqual([
+            CKWSFilterDictionary(comparator: .greaterThan, fieldName: "number", fieldValue: CKWSRecordFieldValue(value: .number(13), type: nil))
+        ], predicate.filterBy)
+    }
+    
+    func testLessThanComparator() {
+        let predicate = Predicate(format: "number < 13")
+        XCTAssertEqual([
+            CKWSFilterDictionary(comparator: .lessThan, fieldName: "number", fieldValue: CKWSRecordFieldValue(value: .number(13), type: nil))
+        ], predicate.filterBy)
+    }
+    
+    func testNotEqualsComparator() {
+        let predicates = [
+            Predicate(format: "number != 13"),
+            Predicate(format: "number <> 13"),
+        ]
+        for predicate in predicates {
+            XCTAssertEqual([
+                CKWSFilterDictionary(comparator: .notEquals, fieldName: "number", fieldValue: CKWSRecordFieldValue(value: .number(13), type: nil))
+            ], predicate.filterBy)
+        }
+    }
+    
     func testValuePredicates() {
         let truePredicate = Predicate(value: true)
         XCTAssertEqual([], truePredicate.filterBy)
     }
     
     static var allTests = [
-        ("testEqualsReference", testEqualsReference),
+        ("testFieldContainsSpecificValue", testFieldContainsSpecificValue),
         ("testMatchFieldToSpecificValue", testMatchFieldToSpecificValue),
         ("testMatchFieldToOneOrMoreValues", testMatchFieldToOneOrMoreValues),
         ("testMatchFieldThatStartsWithStringValue", testMatchFieldThatStartsWithStringValue),
+        ("testEqualsComparator", testEqualsComparator),
+        ("testGreaterThanOrEqualsComparator", testGreaterThanOrEqualsComparator),
+        ("testLessThanOrEqualsComparator", testLessThanOrEqualsComparator),
+        ("testLessThanComparator", testLessThanComparator),
+        ("testGreaterThanComparator", testGreaterThanComparator),
+        ("testNotEqualsComparator", testNotEqualsComparator),
         ("testValuePredicates", testValuePredicates),
     ]
     
